d044710eaf6c72c1372d77d89678c5a01d6a9755
Sandra Katsikis <mindfulripples@gmail.com>
2023-08-17 15:04:36 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random()*10)
+
+const num3 = 3;
+const num4 = 12;
+let mod = num4 / num3;
+
+let max = Math.max(1,7,20);
